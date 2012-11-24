73f46d6f62a73bc3581c23405b1ef316f1e5cedd
Eric McGary <mcgaryes@gmail.com>
2012-11-24 01:03:01 -0500

    Return value for TaskLibrary module now includes only pertinent properties and methods

--- source/Task.js
+++ source/Task.js
@@ -1,185 +1,350 @@
-(function() { 
+/**
+@module TaskLibrary
+*/
+var TaskLibrary = (function() {
 
-	Task = {
+	/**
+	 * description
+	 * @property states {Object} description
+	 */
+	var states = {
+		Initialized:0,
+		Started:1,
+		Canceled:2,
+		Faulted:3,
+		Completed:4
+	};
 
-		extend:function(options){
+	/**
+	 * description
+	 * @property types {Object} description
+	 */
+	var types = {
+		Parallel:"parallel",
+		Sequence:"sequence",
+		Simple:"simple"
+	};
 
-			if(!options)
-			{
-				throw "No options passed";
-				return;
-			}
+	/**
+	 * description
+	 *
+	 * @method create 
+	 * @return 
+	 */
+	var create = function(options){
 
-			var type = options.type;
-			var tasks = options.tasks;
+		if(!options)
+		{
+			throw "No options passed";
+			return;
+		}
 
-			if(type) {
-				if(type == "simple" && (type == "sequence" || type == "parallel")) {
-					return new Task.__SimpleTask__(options);
-				} else {
-					return new Task.__TaskGroup__(options);
-				}
+		var type = options.type;
+		var tasks = options.tasks;
+
+		if(type) {
+			if(type == types.Simple) {
+				return new Task(options);
 			} else {
-				if (!tasks) {
-					return new Task.__SimpleTask__(options);
-				} else {
-					if(tasks.length == 0) {
-						return new Task.__SimpleTask__(options);
-					} else {
-						return new Task.__TaskGroup__(options);
-					}
-				}
+				return new TaskGroup(options);
 			}
-
-		},
-
-		States:{
-			Initialized:0,
-			Started:1,
-			Canceled:2,
-			Faulted:3,
-			Completed:4
-		},
-
-		__SimpleTask__:function(options){
-
-			// setup defaults
-			var name = options.name ? options.name : "SimpleTask";
-			var state = Task.States.Initialized;
-			var type = options.type ? options.type : "simple";
-			var constructor = function(){};
-			var methods = {
-				start:function(){
-					if(this.state >= Task.States.Started) return;
-					this.state = Task.States.Started;
-					if(this.logging) console.log("Started:" + this.name);
-					this.change(this.state);
-					this.perform();
-				},
-				perform:function(){
-					throw "'perform' method should be overridden";
-				},
-				operate:function(operator){
-					if(this.logging) console.log("operate with - " + operator);
-					this.product = operator;
-				},
-				cancel:function(){
-					if(this.state > Task.States.Started) return;
-					this.state = Task.States.Canceled;
-					if(this.logging) console.log("Canceled:" + this.name);
-					this.change(this.state);
-				},
-				complete:function(){
-					if(this.state > Task.States.Started) return;
-					this.state = Task.States.Completed;
-					if(this.logging) console.log("Completed:" + this.name);
-					this.change(this.state);
-				},
-				fault:function(error){
-					if(this.state >= Task.States.Canceled) return;
-					this.state = Task.States.Faulted;
-					if(this.logging) console.log("Faulted:" + this.name);
-					this.change(this.state,error);
-				},
-				change:function(state,error){}
-			}
-
-			// create our task object
-			var proto = new Object();
-			
-			// reset the proto
-			for(var p in proto) if (proto.hasOwnProperty(p)) delete proto[p];
-			
-		    // setup super and name
-			proto.constructor = constructor;
-			proto.name = name;
-			proto.type = type;
-			proto.state = state;
-			
-			// add mixins (e.g. performTask method and name)
-			for(var prop in options) {
-				if(typeof options[prop] == "function" && methods[prop] !== undefined){
-					methods[prop] = options[prop];
-				}else{
-					proto[prop] = options[prop];
-				}
-			}
-
-			// Copy instance methods to the prototype object
-			for(var p in methods) proto[p] = methods[p];
-
-			// set the prototype for the object (task) we're about to return
-			constructor.prototype = proto;
-
-		    // Finally, return the constructor function
-		    return proto;
-
-		},
-
-		__TaskGroup__:function(options){
-
-			var type = options.type ? options.type : "sequence";
-			var tasks = [];
-			var methods = {
-				addSubTask:function(task){
-					throw "task group method 'addSubTask' is not yet implemented";
-				},
-				addSubTaskAfterTask:function(beforeTask,afterTask){
-					throw "task group method 'addSubTaskAfterTask' is not yet implemented";
-				},
-				removeSubTask:function(task){
-					throw "task group method 'removeSubTask' is not yet implemented";
-				},
-				processSubTask:function(task){
-					throw "task group method 'processSubTask' is not yet implemented";
-				},
-				onSubTaskComplete:function(){
-					throw "task group method 'onSubTaskComplete' is not yet implemented";
-				},
-				onSubTaskFault:function(error){
-					throw "task group method 'onSubTaskFault' is not yet implemented";
-				},
-				onSubTaskCancel:function(task){
-					throw "task group method 'onSubTaskCancel' is not yet implemented";
-				},
-				perform:function(){						 
-					throw "task group method 'perform' is not yet implemented";
-				}
-				 
-			};
-
-			// mixin methods from the options and those listed above
-			for(var p in methods) options[p] = methods[p];
-
-			// create tasks for each subtask listed in the array
-			for(var i=0;i<options.tasks.length;i++){
-				var opts = options.tasks[i];
-				var task = Task.extend(opts);
-				tasks.push(task);
-			}
-			options.tasks = tasks;
-
-			// instatiate the correct type of task
-			if(type == "sequence") {
-				return new Task.__SequenceTask__(options);
+		} else {
+			if (!tasks) {
+				return new Task(options);
 			} else {
-				return new Task.__ParallelTask__(options);
+				return new TaskGroup(options);
 			}
-
-		},
-
-		__ParallelTask__:function(){
-			options.type = "parallel";
-			return new Task.__SimpleTask__(options);
-		},
-
-		__SequenceTask__:function(options){
-			options.type = "sequence";
-			return new Task.__SimpleTask__(options);
 		}
 
 	};
 
-	return Task;
+	/**
+	 * description
+	 *
+	 * @method Task 
+	 * @return 
+	 */
+	var Task = function(options){
+		// setup defaults
+		var name = options ? (options.name ? options.name : "Task") : "Task";
+		var state = states.Initialized;
+		var type = options ? (options.type ? options.type : types.Simple) : types.Simple;
+		var logging = options ? (options.logging ? options.logging : false) : false;
+		var constructor = function(){};
+		var methods = {
+			start:function(){
+				if(this.state >= states.Started) return;
+				this.state = states.Started;
+				if(this.logging) console.log("Started:" + this.name);
+				this.change(this.state);
+				this.perform();
+			},
+			perform:function(){
+				throw "'perform' method should be overridden";
+			},
+			cancel:function(){
+				if(this.state > states.Started) return;
+				this.state = states.Canceled;
+				if(this.logging) console.log("Canceled:" + this.name);
+				this.change(this.state);
+			},
+			complete:function(){
+				if(this.state > states.Started) return;
+				this.state = states.Completed;
+				if(this.logging) console.log("Completed:" + this.name);
+				this.change(this.state);
+			},
+			fault:function(error){
+				if(this.state >= states.Canceled) return;
+				this.state = states.Faulted;
+				if(this.logging) console.log("Faulted:" + this.name);
+				this.change(this.state,error);
+			},
+			change:function(state,error){}
+		}
 
-})();
+		// create our task object
+		var proto = new Object();
+		
+		// reset the proto
+		for(var p in proto) if (proto.hasOwnProperty(p)) delete proto[p];
+		
+	    // setup super and name
+		proto.constructor = constructor;
+		proto.name = name;
+		proto.type = type;
+		proto.state = state;
+		proto.logging = logging;
+		
+		// add mixins (e.g. performTask method and name)
+		for(var prop in options) {
+			if(typeof options[prop] == "function" && methods[prop] !== undefined){
+				methods[prop] = options[prop];
+			}else{
+				proto[prop] = options[prop];
+			}
+		}
+
+		// Copy instance methods to the prototype object
+		for(var p in methods) proto[p] = methods[p];
+
+		// set the prototype for the object (task) we're about to return
+		constructor.prototype = proto;
+
+	    // Finally, return the constructor function
+	    return proto;
+
+	}
+	
+	/**
+	 * description
+	 *
+	 * @private
+	 * @method TaskGroup 
+	 * @return 
+	 */
+	var TaskGroup = function(options){
+		var type = options.type ? options.type : types.Sequence;
+		var tasks = [];
+		var methods = {
+			addSubTask:function(task){
+				throw "task group method 'addSubTask' is not yet implemented";
+			},
+			addSubTaskAfterTask:function(beforeTask,afterTask){
+				throw "task group method 'addSubTaskAfterTask' is not yet implemented";
+			},
+			removeSubTask:function(task){
+				throw "task group method 'removeSubTask' is not yet implemented";
+			},
+			processSubTask:function(task){
+				
+				if (!task) throw "You cannot process a task with a nil value.";
+				
+				if(task.state == states.Canceled)
+				{
+					// We are running the sub task canceled functionality here
+					// because there is a chance that the task being about to run
+					// was dependent on a task that was previously canceled in the queue
+					// and we need to make sure that it notifies any tasks that may
+					// be dependent on itself that they also should be canceled
+					//console.log(@"\t--\tRSRCTask:    Skipped:\t%@",task.name);
+					this.onSubTaskCancel(task);
+					return true;
+				}
+				
+				// set concurrency for the sub tasks
+				//task.concurrent = self.concurrent;
+				task.group = this;
+				
+				// set execution block
+				task.change = function(state,error){
+					if(state == states.Completed) this.group.onSubTaskComplete();
+					else if(state == states.Faulted) this.group.onSubTaskFault(error);
+					else if(state == states.Canceled) this.group.onSubTaskCancel(task);
+				}
+				
+				task.start();
+				
+				return false;
+			},
+			onSubTaskComplete:function(){
+				throw "This is an abstract method and must be implemented in a subclass.";
+			},
+			onSubTaskFault:function(error){
+				this.fault(error);
+			},
+			onSubTaskCancel:function(task){
+				this.cancel();
+			},
+			perform:function(){						 
+				throw "This is an abstract method and must be implemented in a subclass.";
+			}
+		};
+
+		// mixin methods from the options and those listed above
+		for(var p in methods) options[p] = methods[p];
+
+		// create tasks for each subtask listed in the array
+		for(var i=0;i<options.tasks.length;i++){
+			var opts = options.tasks[i];
+			// apply any options that we want to be transfered from the group
+			// to the sub tasks (e.g. concurrency, logging)
+			opts.logging = options.logging;
+			var task = create(opts);
+			tasks.push(task);
+		}
+		options.tasks = tasks;
+
+		// instatiate the correct type of task
+		if(type == types.Sequence) {
+			return new SequenceTask(options);
+		} else {
+			return new ParallelTask(options);
+		}
+	};
+
+	/**
+	 * description
+	 *
+	 * @method ParallelTask 
+	 * @return 
+	 */
+	var ParallelTask = function(options) {
+		
+		options.type = types.Parallel;
+		options.currentIndex = 0;
+
+		var methods = {
+			perform:function(){
+				if (this.hasNoEnabledSubTasks()){
+					this.complete();
+				}else{
+					this.processSubTasks();
+				}
+			},
+			hasNoEnabledSubTasks:function(){
+				for (var i = 0; i < this.tasks.length; i++) {
+					var task = this.tasks[i];
+					if(task.state != states.Canceled) return false;
+				};
+				return true;
+			},
+			processSubTasks:function(){
+				for (var i = 0; i < this.tasks.length; i++) {
+					var task = this.tasks[i];
+					this.currentIndex++;
+					this.processSubTask(task);
+				};
+			},
+			addSubTask:function(task) {
+				throw "Not yet implemented";
+				/*
+				if (!task || task.state == RSRCTaskStateCanceled) return;
+				this.currentIndex++;
+				[self.tasks addObject:task];
+				[self processSubTask:task];
+				*/
+			},
+			onSubTaskComplete:function(){
+				if(this.currentIndex-- <= 1) this.complete();
+			}
+		};
+		
+		// mixin methods from the options and those listed above
+		for(var p in methods) options[p] = methods[p];
+		
+		// create a new task based off of the manipulated options
+		return new Task(options);
+
+	};
+
+	/**
+	 * description
+	 *
+	 * @method SequenceTask 
+	 * @return 
+	 */
+	var SequenceTask = function(options){
+		
+		options.type = types.Sequence;
+		options.currentIndex = 0;
+
+		var methods = {
+			 
+			perform:function(){
+				this.startNextSubTask();
+			},
+
+			startNextSubTask:function(){
+				if(this.state >= states.Canceled) return;
+				if (this.tasks && this.currentIndex < this.tasks.length){
+					var skipped = this.processSubTask(this.tasks[this.currentIndex++]);
+					if (skipped) this.startNextSubTask();
+				}else{
+					this.complete();
+				}
+			},
+
+			onSubTaskComplete:function(){
+				if(this.state == states.Canceled) return;
+				this.startNextSubTask();
+			},
+
+			onSubTaskCancel:function(task){
+				//[super onSubTaskCancel:task];
+				if(this.state != states.Canceled) this.startNextSubTask();
+			}
+		};
+
+		// mixin methods from the options and those listed above
+		for(var p in methods) options[p] = methods[p];
+
+		// create a new task based off of the manipulated options
+		return new Task(options);
+	};
+
+	return {
+
+		// propertiess
+		TaskStates:states,
+		TaskTypes:types,
+
+		// methods
+		create:create,
+		Task:Task,
+		SequenceTask:function(options) {
+			if(!options) options = {};
+			options.type = types.Sequence;
+			options.tasks = options.tasks ? options.tasks : [];
+			return new TaskGroup(options);
+		},
+		ParallelTask:function(options) {
+			if(!options) options = {};
+			options.type = types.Parallel;
+			options.tasks = options.tasks ? options.tasks : [];
+			return new TaskGroup(options);
+		}
+
+	};
+
+}());
--- tests/specs/task.spec.js
+++ tests/specs/task.spec.js
@@ -5,7 +5,7 @@ describe("Task Initialization Tests", function() {
 
 	it("Default properties should be set", function() {
 
-		var task = Task.extend({
+		var task = TaskLibrary.create({
 			name:"InitializationTestTask",
 			perform:function(){
 				return "perform";
@@ -24,47 +24,162 @@ describe("Task Initialization Tests", function() {
 	});
 
 	it("Expect 'type' to be simple by default", function() {
-
-		var task = Task.extend({});
+		var task = TaskLibrary.create({});
 		expect(task.type).toEqual("simple");
-
 	});
 
-	it("Expect 'type' to be simple when sub tasks is empty", function() {
-
-		/*
-		var task = Task.extend({
-			type:"parallel",
-			tasks:[]
-		});
-*/
-
-		//expect(task.type).toEqual("simple");
-
-	});
-
-	it("Expect 'type' to be sequence when sub tasks are listed", function() {
-
-/*
-		var task = Task.extend({
+	it("Expect 'type' to be sequence by default when sub tasks are listed", function() {
+		var task = TaskLibrary.create({
 			tasks:[{}]
 		});
-*/
-
-		//expect(task.type).toEqual("sequence");
-
+		expect(task.type).toEqual("sequence");
 	});
 
 	it("Expect 'type' to be parallel when sub tasks are listed and type is set", function() {
-
-/*
-		var task = Task.extend({
+		var task = TaskLibrary.create({
 			type:"parallel",
 			tasks:[{}]
 		});
-*/
-		//expect(task.type).toEqual("parallel");
+		expect(task.type).toEqual("parallel");
+	});
 
+	it("Initializing with ./new syntax creates a simple task that executes",function(){
+		var task = new TaskLibrary.Task({
+			name:"name",
+			perform:function(){
+				this.complete();
+			}
+		});
+		expect(task.type).toEqual("simple");
+		expect(task.name).toEqual("name");
+		expect(task.state).toEqual(0);
+		task.start();
+		expect(task.state).toEqual(4);
+	});
+
+	it("Initializing sequence with ./new syntax creates a simple task that executes",function(){
+		var task = new TaskLibrary.SequenceTask({
+			name:"name",
+			tasks:[{
+				name:"subtask",
+				perform:function(){
+					this.complete();
+				}
+			}],
+			perform:function(){
+				this.complete();
+			}
+		});
+
+		expect(task.type).toEqual("sequence");
+		expect(task.name).toEqual("name");
+		expect(task.state).toEqual(0);
+		task.start();
+		expect(task.state).toEqual(4);
+	});
+
+	it("Initializing parallel with ./new syntax creates a simple task that executes",function(){
+		var task = new TaskLibrary.ParallelTask({
+			name:"name",
+			tasks:[{
+				name:"subtask",
+				perform:function(){
+					this.complete();
+				}
+			}],
+			perform:function(){
+				this.complete();
+			}
+		});
+
+		expect(task.type).toEqual("parallel");
+		expect(task.name).toEqual("name");
+		expect(task.state).toEqual(0);
+		task.start();
+		expect(task.state).toEqual(4);
+	});
+
+	it("Setting simple task properties with . syntax after creation get applied",function(){
+		
+		var task = new TaskLibrary.Task();
+		task.name = "name";
+		task.perform = function(){
+			this.complete();
+		}
+
+		task.start();
+
+		expect(task.type).toEqual("simple");
+		expect(task.name).toEqual("name");
+		expect(task.state).toEqual(4);
+	});
+
+	it("Setting sequence task properties with . syntax after creation get applied",function(){
+		
+		var sequenceTask = new TaskLibrary.SequenceTask();
+		sequenceTask.name = "name";
+		
+		var subtask1 = new TaskLibrary.Task();
+		subtask1.name = "subtask1";
+		subtask1.perform = function(){
+			this.complete();
+		}
+
+		var subtask2 = new TaskLibrary.Task();
+		subtask2.name = "subtask2";
+		subtask2.perform = function(){
+			this.complete();
+		}
+
+		sequenceTask.tasks = [subtask1,subtask2];
+
+		sequenceTask.start();
+
+		expect(sequenceTask.type).toEqual("sequence");
+		expect(sequenceTask.name).toEqual("name");
+		expect(sequenceTask.state).toEqual(4);
+	});
+
+	it("Mixing syntaxes still results in acceptable simple task",function(){
+		
+		var task = new TaskLibrary.Task({
+			name:"name"
+		});
+		task.perform = function(){
+			this.complete();
+		}
+		task.start();
+
+		expect(task.type).toEqual("simple");
+		expect(task.name).toEqual("name");
+		expect(task.state).toEqual(4);
+	});
+
+	it("Mixing syntaxes still results in acceptable group task",function(){
+		
+		var sequenceTask = new TaskLibrary.SequenceTask({
+			name:"name",
+			tasks:[{
+				name:"subtask1",
+				perform:function(){
+					this.complete();
+				}
+			}]
+		});
+		
+		var subtask2 = new TaskLibrary.Task();
+		subtask2.name = "subtask2";
+		subtask2.perform = function(){
+			this.complete();
+		}
+
+		sequenceTask.addSubTask(subtask2);
+
+		sequenceTask.start();
+		expect(sequenceTask.tasks.length).toEqual(2);
+		expect(sequenceTask.type).toEqual("sequence");
+		expect(sequenceTask.name).toEqual("name");
+		expect(sequenceTask.state).toEqual(4);
 	});
 
 });
@@ -74,7 +189,7 @@ describe("Task Execution Cycle Tests", function() {
 	var task;
 
 	beforeEach(function() {
-		task = Task.extend({
+		task = TaskLibrary.create({
 			name:"ExecutionTestTask",
 			perform:function(){}
 		});
@@ -113,52 +228,55 @@ describe("Task Execution Cycle Tests", function() {
 
 });
 
-describe("Task Operation Tests", function() {
-
-	beforeEach(function() {});
-	afterEach(function() {});
-
-	it("Running operate should just set the product", function() {
-		var task = Task.extend({
-			name:"OperatorTestTask",
-			perform:function(){
-				this.operate("b");
-				this.complete();
-			}
-		});
-		task.start();
-		expect(task.product).toEqual("b");
-	});
-
-});
-
 describe("Sequence Task Tests", function() {
 
 	beforeEach(function() {});
 	afterEach(function() {});
 
 	it("Sequence task should complete", function() {
-		var task = Task.extend({
+		var task = TaskLibrary.create({
 			name:"SequenceTaskTests",
 			tasks:[{
 				name:"SubTaskSimple",
 				perform:function(){
 					this.complete();
 				}
-			}],
-			change:function(state,error) {
-				if(state == 4) {
-					console.log("complete");
-				}
-			}
+			}]
 		});
 
-		console.log(task);
+		task.start();
+		expect(task.state).toEqual(4);
+
+	});
+
+	it("Product should be operated on", function() {
+
+		var task = TaskLibrary.create({
+			name:"SequenceTaskTests",
+			product:1,
+			tasks:[{
+				perform:function(){
+					this.group.product = this.group.product/2;
+					this.complete();
+				}
+			},
+			{
+				perform:function(){
+					this.group.product = this.group.product * 4;
+					this.complete();
+				}
+			},
+			{
+				perform:function(){
+					this.group.product = this.group.product * 2;
+					this.complete();
+				}
+			}]
+		});
 
 		task.start();
-		//expect(task.product).toEqual("b");
+		expect(task.product).toEqual(4);
 
 	});
 
 });
-
