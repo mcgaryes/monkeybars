a6e1d9026bdc913296f3ef98ec9b944057f539d0
Eric McGary <emcgary@resource.com>
2013-01-09 21:27:37 -0500

    Adding docs to GitHub pages

---
+++ docs/api.js
@@ -0,0 +1,14 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "MonkeyBars",
+        "ParallelTask",
+        "SequenceTask",
+        "Task",
+        "TaskGroup",
+        "WorkerTask"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
