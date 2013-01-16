0a80db892c04dc9f72319fae8a8c175c5b691a25
Eric McGary <mcgaryes@gmail.com>
2013-01-15 23:14:31 -0500

    Update examples/node_complex/one.js

--- examples/node_complex/one.js
+++ examples/node_complex/one.js
@@ -4,7 +4,6 @@ var MonkeyBars = require("../../monkeybars");
 
 exports.One = MonkeyBars.Task.extend({
 	name:"One",
-	concurrent:true,
 	performTask:function(a) {
 		var newData = 100;
 		this.complete(newData);
